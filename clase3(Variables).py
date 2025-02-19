b095d63684de4d0e68dc7021c7bcc8f30f72f20c
eamv0066 <adesarrollo4@unico.com.co>
2025-02-19 07:48:19 -0500

    Mi primer commit

---
+++ clase3(Variables).py
@@ -0,0 +1,4 @@
+saludo_1="Hola"
+saludo_2="Hi"
+nombre="Edwin"
+print(saludo_1)
---
+++ clase4(ManipulacionCadenas).py
@@ -0,0 +1,22 @@
+'Salto de linea'
+name='''Carli
+
+Vanegas'''
+print((name))
+
+'Indexacion'
+name2='Edwin'
+lastname='Mendez'
+print(name2[4])
+print(name2)
+print(lastname)
+print(name2 + " " + lastname)
+
+"Replicacion"
+print(5*name2)
+
+"Longitud"
+print(len(lastname))
+
+"Mayuscula y minuscula"
+print(name2.upper())
